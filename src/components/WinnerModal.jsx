76f622fba8e04e19ea7c4a5d9eeaed0879c17246
patrickwebs <patricioalmada911@gmail.com>
2023-04-10 21:42:42 -0300

    Eslint test changes

--- src/components/WinnerModal.jsx
+++ src/components/WinnerModal.jsx
@@ -1,20 +1,16 @@
-import { Square } from "./Square.jsx";
+import { Square } from './Square.jsx'
 
-export function WinnerModal({winner, resetGame}) {
-    if(winner === null) return null;
-    return (
-        <section className="winner">
-        <div className="text">
-            <h2>
-            { winner === false ? 'Empate' : ' Ganó: ' }
-            </h2>
-            <header className="win">
-            {winner && <Square>{winner}</Square>}
-            </header>
-            <footer>
-            <button onClick={resetGame}>Empezar de nuevo</button>
-            </footer>
-        </div>
-        </section>
-    )
+export function WinnerModal ({ winner, resetGame }) {
+  if (winner === null) return null
+  return (
+    <section className='winner'>
+      <div className='text'>
+        <h2>{winner === false ? 'Empate' : ' Ganó: '}</h2>
+        <header className='win'>{winner && <Square>{winner}</Square>}</header>
+        <footer>
+          <button onClick={resetGame}>Empezar de nuevo</button>
+        </footer>
+      </div>
+    </section>
+  )
 }
